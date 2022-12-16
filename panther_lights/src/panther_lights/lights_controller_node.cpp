ea3ed1a615425519d88cbe3d301a4f7a2edd8151
Dawid <kmakd197@gmail.com>
2022-12-16 14:30:48 +0100

    rename package

--- panther_lights/src/panther_lights/lights_controller_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "LightsController.hpp"
-
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "LightsControllerNode");
-    ros::NodeHandle nh("");
-    LightsController lc(nh);
-    ros::spin();
-}
