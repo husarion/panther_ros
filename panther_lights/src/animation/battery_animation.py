3709577d638238b56787f940c0e1acf5bb279418
Dawid <kmakd197@gmail.com>
2023-01-18 14:39:05 +0100

    fix battery animation

--- panther_lights/src/animation/battery_animation.py
+++ panther_lights/src/animation/battery_animation.py
@@ -26,6 +26,7 @@ class BatteryAnimation(Animation):
         self._create_anim(value)
 
     def _create_anim(self, battery_percent: float) -> None:
+        battery_percent = np.clip(battery_percent, 0, 1)
         # define basic HSV color
         h = self._h_min
         s = 1.0
