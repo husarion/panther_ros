3479d324b232d02da9a728563db9bc966310130a
rafal-gorecki <rafal.gorecki@husarion.com>
2023-04-13 12:10:48 +0200

    formated

--- panther_lights/src/animation/battery_animation.py
+++ panther_lights/src/animation/battery_animation.py
@@ -3,16 +3,17 @@ import numpy as np
 from PIL import Image, ImageFilter
 
 from animation import Animation
-class BatteryAnimation(Animation):
 
+
+class BatteryAnimation(Animation):
     ANIMATION_NAME = 'battery_animation'
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._anim = np.zeros((self._anim_len, self.num_led))
-        self._h_min = 0.0       # red color
-        self._h_max = 0.4       # green color
+        self._h_min = 0.0  # red color
+        self._h_max = 0.4  # green color
         self._resolution = 100
         self._start_fade = 0.80 * self._resolution
         self._end_anim = 0.90 * self._resolution
@@ -54,7 +55,9 @@ class BatteryAnimation(Animation):
                     frame.fill(0)
                     ind_1 = int(np.ceil(self._num_led / 2.0 - anim_ind - 1.0))
                     ind_2 = int(np.floor(self._num_led / 2.0 + anim_ind))
-                    h = (self._h_max - self._h_min) * np.sin(battery_percent * np.pi / 2.0) * i / (display_iterations / 2.0) + self._h_min
+                    h = (self._h_max - self._h_min) * np.sin(battery_percent * np.pi / 2.0) * i / (
+                        display_iterations / 2.0
+                    ) + self._h_min
                     rgb = np.array(hsv_to_rgb(h, 1.0, 1.0))
                     frame[ind_1] = np.uint8(rgb * 255.0)
                     frame[ind_2] = np.uint8(rgb * 255.0)
@@ -69,9 +72,9 @@ class BatteryAnimation(Animation):
             anim[i] = frame
 
             if i > self._start_fade:
-                a = 1/(self._start_fade - self._end_anim)
-                b = -a*self._end_anim
-                coef = max(0, (a*i + b))
+                a = 1 / (self._start_fade - self._end_anim)
+                b = -a * self._end_anim
+                coef = max(0, (a * i + b))
                 anim[i] = frame * coef
 
         # filter to smooth animation and resize to match duration
