3d31a6755b143f43dda674832d7e931fe70b449b
Krzysztof Wojciechowski <49921081+Kotochleb@users.noreply.github.com>
2023-05-10 13:08:40 +0200

    APA102 cpp implementation (#108)
    * Use CPP for APA102
    
    * Add color correction
    
    * Utilise gpiod
    
    * Remove ::
    
    * Fix lights after tests
    
    * Fix default pin number
    
    * Add set brightness service
    
    * Invert LED_SBC_SEL pin
    
    * Change SPI mode
    
    * Change clock polarity
    
    * Reduce number of ROS lgger instances
    
    * Celan up code, update to libgpiod 2.0
    
    * Update panther_lights/include/panther_lights/apa102.hpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Update panther_lights/src/lights_driver_node.cpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Update panther_lights/src/lights_driver_node.cpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Update panther_lights/include/panther_lights/apa102.hpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Review changes
    
    * Update panther_lights/src/apa102.cpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Review cleanup
    
    * Add build dependencies
    
    * FIx launchfile
    
    * Update panther_lights/CMakeLists.txt
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Update panther_lights/CMakeLists.txt
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Update panther_lights/src/apa102.cpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Update panther_lights/src/driver_node.cpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Update panther_lights/src/driver_node.cpp
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>
    
    * Review changes
    
    * Fix inlude order
    
    * Fix references and APA102 constructor
    
    * Add build dependencies for libgpiod v2
    
    * Bring back libgpiod v1
    
    ---------
    
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>

---
+++ panther_lights/include/panther_lights/apa102.hpp
@@ -0,0 +1,37 @@
+#ifndef PANTHER_LIGHTS_APA102_HPP_
+#define PANTHER_LIGHTS_APA102_HPP_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace panther_lights
+{
+
+class APA102
+{
+public:
+  APA102(
+    const std::string & device, const std::uint32_t speed = 800000, const bool cs_high = false);
+  ~APA102();
+
+  void set_global_brightness(const std::uint8_t brightness);
+  void set_global_brightness(const double brightness);
+  void set_panel(const std::vector<std::uint8_t> & frame) const;
+
+private:
+  const int fd_;
+  const std::string device_;
+  const std::uint8_t bits_ = 8;
+  const std::uint32_t speed_;
+  std::uint16_t global_brightness_;
+
+  // color correction constants
+  const std::uint16_t corr_red_ = 255;
+  const std::uint16_t corr_green_ = 200;
+  const std::uint16_t corr_blue_ = 62;
+};
+
+}  // namespace panther_lights
+
+#endif  // PANTHER_LIGHTS_APA102_HPP_
---
+++ panther_lights/src/driver_node.cpp
@@ -0,0 +1,139 @@
+#include <panther_lights/driver_node.hpp>
+
+#include <filesystem>
+#include <memory>
+
+#include <gpiod.hpp>
+#include <ros/ros.h>
+
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <panther_msgs/SetLEDBrightness.h>
+
+#include <panther_lights/apa102.hpp>
+
+namespace panther_lights
+{
+
+DriverNode::DriverNode(
+  const std::shared_ptr<ros::NodeHandle> & ph, std::shared_ptr<ros::NodeHandle> & nh,
+  const std::shared_ptr<image_transport::ImageTransport> & it)
+: ph_(std::move(ph)),
+  nh_(std::move(nh)),
+  it_(std::move(it)),
+  front_panel_("/dev/spidev0.0"),
+  rear_panel_("/dev/spidev0.1")
+{
+  node_name_ = ros::this_node::getName();
+
+  const double global_brightness = ph_->param<double>("global_brightness", 1.0);
+  frame_timeout_ = ph_->param<double>("frame_timeout", 0.1);
+  num_led_ = ph_->param<int>("num_led", 46);
+
+  const gpiod::chip chip("gpiochip0");
+  power_pin_ = chip.find_line("LED_SBC_SEL");
+
+  const gpiod::line_request lr = {
+    node_name_, gpiod::line_request::DIRECTION_OUTPUT, gpiod::line_request::FLAG_ACTIVE_LOW};
+  power_pin_.request(lr, 0);
+
+  front_panel_ts_ = ros::Time::now();
+  rear_panel_ts_ = ros::Time::now();
+
+  front_panel_.set_global_brightness(global_brightness);
+  rear_panel_.set_global_brightness(global_brightness);
+
+  front_light_sub_ = it_->subscribe(
+    "lights/driver/front_panel_frame", 5, [&](const sensor_msgs::Image::ConstPtr & msg) {
+      frame_cb(msg, front_panel_, front_panel_ts_, "front");
+      front_panel_ts_ = msg->header.stamp;
+    });
+
+  rear_light_sub_ = it_->subscribe(
+    "lights/driver/rear_panel_frame", 5, [this](const sensor_msgs::Image::ConstPtr & msg) {
+      frame_cb(msg, rear_panel_, rear_panel_ts_, "rear");
+      rear_panel_ts_ = msg->header.stamp;
+    });
+
+  set_brightness_server_ =
+    nh_->advertiseService("lights/driver/set/brightness", &DriverNode::set_brightness_cb, this);
+
+  while (ros::ok() && !panels_initialised_) {
+    ROS_INFO_THROTTLE(5.0, "[%s] Waiting for animation to arrive...", node_name_.c_str());
+    ros::Duration(1.0 / 30.0).sleep();
+    ros::spinOnce();
+  }
+
+  ROS_INFO("[%s] LED panels initialised", node_name_.c_str());
+  ROS_INFO("[%s] Node started", node_name_.c_str());
+}
+
+DriverNode::~DriverNode()
+{
+  // clear LEDs
+  front_panel_.set_panel(std::vector<std::uint8_t>(num_led_ * 4, 0));
+  rear_panel_.set_panel(std::vector<std::uint8_t>(num_led_ * 4, 0));
+
+  // give back control over LEDs
+  power_pin_.set_value(0);
+  power_pin_.release();
+}
+
+bool DriverNode::set_brightness_cb(
+  panther_msgs::SetLEDBrightness::Request & req, panther_msgs::SetLEDBrightness::Response & res)
+{
+  float brightness = req.data;
+  if (brightness < 0.0f || brightness > 1.0f) {
+    res.success = false;
+    res.message = "Brightness out of range <0,1>";
+    return true;
+  }
+  front_panel_.set_global_brightness(brightness);
+  rear_panel_.set_global_brightness(brightness);
+  auto str_bright = std::to_string(brightness);
+
+  // round string to two decimal places
+  str_bright = str_bright.substr(0, str_bright.find(".") + 3);
+  res.success = true;
+  res.message = "Changed brightness to " + str_bright;
+  return true;
+}
+
+void DriverNode::frame_cb(
+  const sensor_msgs::Image::ConstPtr & msg, const APA102 & panel, const ros::Time & last_time,
+  const std::string & panel_name)
+{
+  std::string meessage;
+  if ((ros::Time::now() - msg->header.stamp).toSec() > frame_timeout_) {
+    meessage = "Timeout exceeded, ignoring frame";
+  } else if (msg->header.stamp < last_time) {
+    meessage = "Dropping message from past";
+  } else if (msg->encoding != sensor_msgs::image_encodings::RGBA8) {
+    meessage = "Incorrect image encoding ('" + msg->encoding + "')";
+  } else if (msg->height != 1) {
+    meessage = "Incorrect image height " + std::to_string(msg->height);
+  } else if (msg->width != num_led_) {
+    meessage = "Incorrect image width " + std::to_string(msg->width);
+  } else if ((ros::Time::now() - msg->header.stamp).toSec() > 5.0) {
+    meessage = "Timeout. Dropping frame";
+  }
+
+  if (!meessage.empty()) {
+    if (panel_name == "front") {
+      ROS_WARN_THROTTLE(5.0, "[%s] %s on front panel!", node_name_.c_str(), meessage.c_str());
+    } else if (panel_name == "rear") {
+      ROS_WARN_THROTTLE(5.0, "[%s] %s on front rear!", node_name_.c_str(), meessage.c_str());
+    }
+  } else {
+    if (!panels_initialised_) {
+      panels_initialised_ = true;
+
+      // take control over LEDs
+      power_pin_.set_value(1);
+    }
+    panel.set_panel(msg->data);
+  }
+}
+
+}  // namespace panther_lights
