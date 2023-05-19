fa4a61e03126b98660dd56ff94370bdb51326cf2
Dawid <kmakd197@gmail.com>
2023-05-19 15:07:46 +0200

    hosts list as shared_ptr

--- panther_manager/include/panther_manager/plugins/shutdown_hosts_node.hpp
+++ panther_manager/include/panther_manager/plugins/shutdown_hosts_node.hpp
@@ -32,10 +32,10 @@ public:
   virtual ~ShutdownHosts() = default;
 
   // method to be implemented by user
-  virtual void update_hosts(std::vector<ShutdownHost> & hosts) = 0;
+  virtual void update_hosts(std::vector<std::shared_ptr<ShutdownHost>> & hosts) = 0;
 
   // method that can be overriden by user
-  virtual BT::NodeStatus on_end()
+  virtual BT::NodeStatus post_process()
   {
     // return success only when all hosts succeded
     if (failed_hosts_.size() == 0) return BT::NodeStatus::SUCCESS;
@@ -43,14 +43,16 @@ public:
   }
 
   std::string get_node_name() const { return node_name_; }
-  std::vector<std::shared_ptr<ShutdownHost>> const get_failed_hosts() { return failed_hosts_; }
+  std::vector<std::size_t> const get_failed_hosts() { return failed_hosts_; }
 
 private:
-  int host_index_ = 0;
+  int check_host_index_ = 0;
   std::string node_name_;
-  std::vector<ShutdownHost> hosts_;
-  std::vector<std::shared_ptr<ShutdownHost>> hosts_to_check_;
-  std::vector<std::shared_ptr<ShutdownHost>> failed_hosts_;
+  std::vector<std::shared_ptr<ShutdownHost>> hosts_;
+  std::vector<std::size_t> hosts_to_check_;
+  std::vector<std::size_t> skipped_hosts_;
+  std::vector<std::size_t> succeeded_hosts_;
+  std::vector<std::size_t> failed_hosts_;
 
   BT::NodeStatus onStart()
   {
@@ -60,70 +62,72 @@ private:
       ROS_ERROR("[%s] Hosts list is empty! Check configuration!", node_name_.c_str());
       return BT::NodeStatus::FAILURE;
     }
-    for (auto & host : hosts_) {
-      hosts_to_check_.push_back(std::make_shared<ShutdownHost>(host));
+    for (std::size_t i = 0; i < hosts_.size(); i++) {
+      hosts_to_check_.push_back(i);
     }
     return BT::NodeStatus::RUNNING;
   }
 
   BT::NodeStatus onRunning()
   {
-    if (hosts_to_check_.size() <= 0) return on_end();
+    if (hosts_to_check_.size() <= 0) return post_process();
 
-    if (host_index_ >= hosts_to_check_.size()) host_index_ = 0;
+    if (check_host_index_ >= hosts_to_check_.size()) check_host_index_ = 0;
 
-    auto host = hosts_to_check_.at(host_index_);
+    auto host_index = hosts_to_check_.at(check_host_index_);
+    auto host = hosts_[host_index];
     host->call();
 
     switch (host->get_state()) {
       case ShutdownHostState::RESPONSE_RECEIVED:
-
         ROS_INFO(
           "[%s] Device at: %s response:\n%s", node_name_.c_str(), host->get_ip().c_str(),
           host->get_response().c_str());
-        host_index_++;
+        check_host_index_++;
         break;
 
       case ShutdownHostState::SUCCESS:
         ROS_INFO(
           "[%s] Successfuly shutdown device at: %s", node_name_.c_str(), host->get_ip().c_str());
-        hosts_to_check_.erase(hosts_to_check_.begin() + host_index_);
+        succeeded_hosts_.push_back(host_index);
+        hosts_to_check_.erase(hosts_to_check_.begin() + check_host_index_);
         break;
 
       case ShutdownHostState::FAILURE:
         ROS_WARN(
           "[%s] Failed to shutdown device at: %s\n%s", node_name_.c_str(), host->get_ip().c_str(),
           host->get_error().c_str());
-        failed_hosts_.push_back(host);
-        hosts_to_check_.erase(hosts_to_check_.begin() + host_index_);
+        failed_hosts_.push_back(host_index);
+        hosts_to_check_.erase(hosts_to_check_.begin() + check_host_index_);
         break;
 
       case ShutdownHostState::SKIPPED:
         ROS_WARN(
           "[%s] Davice at: %s not available, skipping", node_name_.c_str(), host->get_ip().c_str());
-        hosts_to_check_.erase(hosts_to_check_.begin() + host_index_);
+        skipped_hosts_.push_back(host_index);
+        hosts_to_check_.erase(hosts_to_check_.begin() + check_host_index_);
         break;
 
       default:
-        host_index_++;
+        check_host_index_++;
         break;
     }
 
     return BT::NodeStatus::RUNNING;
   }
 
-  void remove_duplicate_hosts(std::vector<ShutdownHost> & hosts)
+  void remove_duplicate_hosts(std::vector<std::shared_ptr<ShutdownHost>> & hosts)
   {
     std::set<ShutdownHost> seen;
-    std::vector<ShutdownHost> result;
+    std::vector<std::shared_ptr<ShutdownHost>> result;
 
     for (const auto & host : hosts) {
-      if (!seen.count(host)) {
-        seen.insert(host);
+      if (!seen.count(*host)) {
+        seen.insert(*host);
         result.push_back(host);
       } else {
         ROS_WARN(
-          "Found duplicate host: %s\nProcessing only the first occurence.", host.get_ip().c_str());
+          "Found duplicate host: %s\nProcessing only the first occurence.", host->get_ip().c_str());
       }
     }
 
@@ -133,7 +137,7 @@ private:
   void onHalted()
   {
     for (auto & host : hosts_) {
-      host.close_connection();
+      host->close_connection();
     }
   }
 };
