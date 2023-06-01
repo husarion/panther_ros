3c5d64bbfb4643f51202364f478d1164fa6d58eb
Dawid <kmakd197@gmail.com>
2023-06-01 16:14:59 +0200

    add MovingAverage

---
+++ panther_utils/include/panther_utils/moving_average.hpp
@@ -0,0 +1,46 @@
+#ifndef PANTHER_UTILS_MOVING_AVERAGE_HPP_
+#define PANTHER_UTILS_MOVING_AVERAGE_HPP_
+
+#include <deque>
+
+namespace panther_utils
+{
+
+template <typename T>
+class MovingAverage
+{
+public:
+  MovingAverage(const unsigned window_size = 5, const T initial_value = T(0))
+  : window_size_(window_size), initial_value_(initial_value)
+  {
+  }
+
+  void Roll(const T value)
+  {
+    values_.push_back(value);
+    sum_ += value;
+
+    if (values_.size() > window_size_) {
+      sum_ -= values_.front();
+      values_.pop_front();
+    }
+  }
+
+  T GetAverage() const
+  {
+    if (values_.size() == 0) {
+      return initial_value_;
+    }
+    return sum_ / static_cast<T>(values_.size());
+  }
+
+private:
+  const unsigned window_size_;
+  std::deque<T> values_;
+  const T initial_value_;
+  T sum_;
+};
+
+}  // namespace panther_utils
+
+#endif  // PANTHER_UTILS_MOVING_AVERAGE_HPP_
