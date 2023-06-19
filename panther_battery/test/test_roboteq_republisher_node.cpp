f1b4438ab7f377ab15d044603b30777d85cdf4c2
Dawid <kmakd197@gmail.com>
2023-06-19 14:18:35 +0000

    add test utils

--- panther_battery/test/test_roboteq_republisher_node.cpp
+++ panther_battery/test/test_roboteq_republisher_node.cpp
@@ -9,8 +9,11 @@
 #include <panther_msgs/msg/driver_state.hpp>
 
 #include <panther_battery/roboteq_republisher_node.hpp>
+#include <panther_utils/test/test_utils.hpp>
 
 using namespace std::chrono_literals;
+using namespace panther_utils::test_utils;
+
 using BatteryStateMsg = sensor_msgs::msg::BatteryState;
 using DriverStateMsg = panther_msgs::msg::DriverState;
 
@@ -21,60 +24,31 @@ public:
   ~TestRoboteqRepublisherNode();
 
 protected:
-  bool WaitForBatteryStateMsg(const std::chrono::milliseconds & timeout);
   void CheckBatteryStateMsg(
     const float & expected_voltage, const float & expected_current,
     const uint8_t & power_supply_status, const uint8_t & power_supply_health);
 
   BatteryStateMsg::SharedPtr battery_state_;
-  rclcpp::Node::SharedPtr test_node_;
   rclcpp::Publisher<DriverStateMsg>::SharedPtr driver_state_pub_;
   rclcpp::Subscription<BatteryStateMsg>::SharedPtr battery_state_sub_;
   panther_battery::RoboteqRepublisherNode::SharedPtr roboteq_republisher_node_;
-
-  rclcpp::Executor::SharedPtr executor_;
-  std::thread executor_thread_;
 };
 
 TestRoboteqRepublisherNode::TestRoboteqRepublisherNode()
 {
-  test_node_ = std::make_shared<rclcpp::Node>("test_node");
   roboteq_republisher_node_ = std::make_shared<panther_battery::RoboteqRepublisherNode>();
 
   driver_state_pub_ =
-    test_node_->create_publisher<DriverStateMsg>("driver/motor_controllers_state", 1);
-  battery_state_sub_ = test_node_->create_subscription<BatteryStateMsg>(
+    roboteq_republisher_node_->create_publisher<DriverStateMsg>("driver/motor_controllers_state", 1);
+  battery_state_sub_ = roboteq_republisher_node_->create_subscription<BatteryStateMsg>(
     "battery", 10, [&](const BatteryStateMsg::SharedPtr msg) { battery_state_ = msg; });
-
-  executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-  executor_->add_node(test_node_);
-  executor_->add_node(roboteq_republisher_node_);
-  executor_thread_ = std::thread([&]() { this->executor_->spin(); });
 }
 
 TestRoboteqRepublisherNode::~TestRoboteqRepublisherNode()
 {
-  executor_->cancel();
-  executor_thread_.join();
-
-  test_node_.reset();
   roboteq_republisher_node_.reset();
   driver_state_pub_.reset();
   battery_state_sub_.reset();
-}
-
-bool TestRoboteqRepublisherNode::WaitForBatteryStateMsg(const std::chrono::milliseconds & timeout)
-{
-  battery_state_ = nullptr;
-  auto start_time = test_node_->now();
-
-  while (rclcpp::ok() && (test_node_->now() - start_time) < rclcpp::Duration(timeout)) {
-    if (battery_state_) {
-      return true;
-    }
-    std::this_thread::sleep_for(10ms);
-  }
-  return false;
 }
 
 void TestRoboteqRepublisherNode::CheckBatteryStateMsg(
@@ -105,7 +79,8 @@ void TestRoboteqRepublisherNode::CheckBatteryStateMsg(
 
 TEST_F(TestRoboteqRepublisherNode, BatteryMsgDefaultValues)
 {
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   CheckBatteryStateMsg(
     0.0, 0.0, BatteryStateMsg::POWER_SUPPLY_STATUS_UNKNOWN,
@@ -121,7 +96,8 @@ TEST_F(TestRoboteqRepublisherNode, BatteryMsgValues)
   driver_state_msg.rear.current = 0.1;
 
   driver_state_pub_->publish(driver_state_msg);
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   auto expected_voltage = (driver_state_msg.front.voltage + driver_state_msg.rear.voltage) / 2;
   auto expected_current = driver_state_msg.front.current + driver_state_msg.rear.current;
@@ -139,7 +115,8 @@ TEST_F(TestRoboteqRepublisherNode, BatteryMsgBatteryDead)
   driver_state_msg.rear.current = 0.1;
 
   driver_state_pub_->publish(driver_state_msg);
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   auto expected_voltage = (driver_state_msg.front.voltage + driver_state_msg.rear.voltage) / 2;
   auto expected_current = driver_state_msg.front.current + driver_state_msg.rear.current;
@@ -157,7 +134,8 @@ TEST_F(TestRoboteqRepublisherNode, BatteryMsgBatteryOvervoltage)
   driver_state_msg.rear.current = 0.1;
 
   driver_state_pub_->publish(driver_state_msg);
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   auto expected_voltage = (driver_state_msg.front.voltage + driver_state_msg.rear.voltage) / 2;
   auto expected_current = driver_state_msg.front.current + driver_state_msg.rear.current;
@@ -175,7 +153,8 @@ TEST_F(TestRoboteqRepublisherNode, BatteryMsgTimoeut)
   driver_state_msg.rear.current = 0.1;
 
   driver_state_pub_->publish(driver_state_msg);
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   // check if message was send correctly
   auto expected_voltage = (driver_state_msg.front.voltage + driver_state_msg.rear.voltage) / 2;
@@ -186,7 +165,8 @@ TEST_F(TestRoboteqRepublisherNode, BatteryMsgTimoeut)
 
   // sleep and check timeout
   std::this_thread::sleep_for(2000ms);
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   CheckBatteryStateMsg(
     0.0, 0.0, BatteryStateMsg::POWER_SUPPLY_STATUS_UNKNOWN,
@@ -202,7 +182,8 @@ TEST_F(TestRoboteqRepublisherNode, DriverStateMsgCANNetError)
   driver_state_msg.rear.current = 0.1;
 
   driver_state_pub_->publish(driver_state_msg);
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   // check if message was send correctly
   auto expected_voltage = (driver_state_msg.front.voltage + driver_state_msg.rear.voltage) / 2;
@@ -218,11 +199,12 @@ TEST_F(TestRoboteqRepublisherNode, DriverStateMsgCANNetError)
   driver_state_msg.front.current = 0.2;
   driver_state_msg.rear.current = 0.2;
   driver_state_pub_->publish(driver_state_msg);
-  ASSERT_TRUE(WaitForBatteryStateMsg(std::chrono::milliseconds(1000)));
+  ASSERT_TRUE(
+    WaitForMsg(roboteq_republisher_node_, battery_state_, std::chrono::milliseconds(1000)));
 
   // voltage and current values should not be updated
   CheckBatteryStateMsg(
-    37.0, 0.2, BatteryStateMsg::POWER_SUPPLY_STATUS_DISCHARGING,
+    expected_voltage, expected_current, BatteryStateMsg::POWER_SUPPLY_STATUS_DISCHARGING,
     BatteryStateMsg::POWER_SUPPLY_HEALTH_GOOD);
 }
 
---
+++ panther_utils/include/panther_utils/test/test_utils.hpp
@@ -0,0 +1,34 @@
+#ifndef PANTHER_UTILS_TEST_UTILS_HPP_
+#define PANTHER_UTILS_TEST_UTILS_HPP_
+
+#include <chrono>
+#include <memory>
+
+#include <rclcpp/rclcpp.hpp>
+
+using namespace std::chrono_literals;
+
+namespace panther_utils::test_utils
+{
+
+template <typename NodeT, typename MsgT>
+bool WaitForMsg(
+  const std::shared_ptr<NodeT> node, std::shared_ptr<MsgT> & msg,
+  const std::chrono::nanoseconds & timeout)
+{
+  msg = nullptr;
+  rclcpp::Time start_time = node->now();
+
+  while (rclcpp::ok() && node->now() - start_time <= rclcpp::Duration(timeout)) {
+    if (msg) {
+      return true;
+    }
+    rclcpp::spin_some(node->get_node_base_interface());
+    std::this_thread::sleep_for(10ms);
+  }
+  return false;
+}
+
+}  // namespace panther_utils::test_utils
+
+#endif  // PANTHER_UTILS_TEST_UTILS_HPP_
