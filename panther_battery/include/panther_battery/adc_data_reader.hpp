96a621e86be74a28c6d48ca8ac089ea4867673c7
Dawid <kmakd197@gmail.com>
2023-07-17 11:28:35 +0000

    data reader fixes

--- panther_battery/include/panther_battery/adc_data_reader.hpp
+++ panther_battery/include/panther_battery/adc_data_reader.hpp
@@ -1,11 +1,8 @@
 #ifndef PANTHER_BATTERY_ADC_DATA_READER_HPP_
 #define PANTHER_BATTERY_ADC_DATA_READER_HPP_
 
-#include <math.h>
-
 #include <filesystem>
 #include <fstream>
-#include <map>
 #include <stdexcept>
 #include <string>
 
@@ -15,27 +12,33 @@ namespace panther_battery
 class ADCDataReader
 {
 public:
-  ADCDataReader(const std::string & device_path) : device_path_(device_path) {}
+  ADCDataReader(const std::string & device_path) : device_path_(device_path)
+  {
+    if (!std::filesystem::exists(device_path_)) {
+      throw std::runtime_error("Failed to open device path: " + std::string(device_path_));
+    }
+  }
 
-  float GetADCMeasurement(const std::string & file, const float & offset, const float & LSB);
+  float GetADCMeasurement(const std::string & channel, const float offset, const float LSB);
 
 private:
-  const std::string device_path_;
+  const std::filesystem::path device_path_;
 
-  int ReadFile(std::string file_path);
+  int ReadChannel(std::string channel);
 };
 
 inline float ADCDataReader::GetADCMeasurement(
-  const std::string & file, const float & offset, const float & LSB)
+  const std::string & channel, const float offset, const float LSB)
 {
-  auto filepath = std::filesystem::path(device_path_) / std::filesystem::path(file);
-  auto raw_value = ReadFile(filepath);
+  auto raw_value = ReadChannel(channel);
   return (raw_value - offset) * LSB;
 }
 
-inline int ADCDataReader::ReadFile(std::string file_path)
+inline int ADCDataReader::ReadChannel(std::string channel)
 {
   int data;
+  auto file_path = device_path_ / std::filesystem::path(channel);
+
   std::fstream file(file_path, std::ios_base::in);
   if (!file) {
     throw std::runtime_error("Failed to open file: " + std::string(file_path));
