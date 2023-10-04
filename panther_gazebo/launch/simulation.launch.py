d593a95e935d161e7d418edc694b3d1bdd4f096f
Paweł Kowalski <82044322+pkowalsk1@users.noreply.github.com>
2023-10-04 15:23:13 +0200

    Update panther_gazebo/launch/simulation.launch.py
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>

--- panther_gazebo/launch/simulation.launch.py
+++ panther_gazebo/launch/simulation.launch.py
@@ -6,9 +6,9 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
-    PythonExpression,
-    PathJoinSubstitution,
     LaunchConfiguration,
+    PathJoinSubstitution,
+    PythonExpression,
 )
 
 from launch_ros.actions import Node, SetParameter
