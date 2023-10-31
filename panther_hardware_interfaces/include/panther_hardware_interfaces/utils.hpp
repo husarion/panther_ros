c44310688d114ddd339468e680e8d22443cd5e12
Maciej Stępień <maciej.stepien@husarion.com>
2023-10-31 10:43:03 +0000

    Remove unnecessary headers

--- panther_hardware_interfaces/include/panther_hardware_interfaces/utils.hpp
+++ panther_hardware_interfaces/include/panther_hardware_interfaces/utils.hpp
@@ -1,16 +1,6 @@
 #ifndef PANTHER_HARDWARE_INTERFACES__UTILS_HPP_
 #define PANTHER_HARDWARE_INTERFACES__UTILS_HPP_
 
-#include <atomic>
-#include <condition_variable>
-#include <vector>
-
-#include <lely/coapp/fiber_driver.hpp>
-
-#include <panther_msgs/msg/fault_flag.hpp>
-#include <panther_msgs/msg/script_flag.hpp>
-#include <panther_msgs/msg/runtime_error.hpp>
-
 namespace panther_hardware_interfaces
 {
 
