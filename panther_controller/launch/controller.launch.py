a1942a75d8eba99dedef87232ac3dc25e272cee3
Maciej Stępień <maciej.stepien@husarion.com>
2023-12-15 15:13:14 +0000

    Remove hardcoded panther namespace

--- panther_controller/launch/controller.launch.py
+++ panther_controller/launch/controller.launch.py
@@ -132,10 +132,10 @@ def generate_launch_description():
         remappings=[
             ("panther_base_controller/cmd_vel_unstamped", "/cmd_vel"),
             ("panther_base_controller/odom", "odom/wheels"),
-            ("panther_system_node/io_state", "/panther/hardware/io_state"),
-            ("panther_system_node/e_stop", "/panther/hardware/e_stop"),
-            ("panther_system_node/e_stop_trigger", "/panther/hardware/e_stop_trigger"),
-            ("panther_system_node/e_stop_reset", "/panther/hardware/e_stop_reset"),
+            ("panther_system_node/io_state", "hardware/io_state"),
+            ("panther_system_node/e_stop", "hardware/e_stop"),
+            ("panther_system_node/e_stop_trigger", "hardware/e_stop_trigger"),
+            ("panther_system_node/e_stop_reset", "hardware/e_stop_reset"),
         ],
         condition=UnlessCondition(use_sim),
     )
