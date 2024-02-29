cb144a62136610566c1a99bf66689d9cce8071f9
Paweł Irzyk <108666440+pawelirh@users.noreply.github.com>
2024-02-29 15:43:49 +0100

    Update panther_utils/test/test_diagnostics.cpp
    Co-authored-by: Dawid Kmak <73443304+KmakD@users.noreply.github.com>

--- panther_utils/test/test_diagnostics.cpp
+++ panther_utils/test/test_diagnostics.cpp
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <map>
+#include <memory>
+#include <string>
+
 #include <gtest/gtest.h>
 
 #include <diagnostic_updater/diagnostic_status_wrapper.hpp>
