352fb476809668602ce1bd7cbf597c1ed019a7fc
rafal-gorecki <rafal.gorecki@husarion.com>
2024-03-27 18:06:28 +0100

    pre-commit

--- panther_lights/test/test_driver_node.cpp
+++ panther_lights/test/test_driver_node.cpp
@@ -28,7 +28,11 @@ using SetLEDBrightnessSrv = panther_msgs::srv::SetLEDBrightness;
 class DriverNodeWrapper : public panther_lights::DriverNode
 {
 public:
-  DriverNodeWrapper(const std::string & node_name, const rclcpp::NodeOptions & options = rclcpp::NodeOptions()) : DriverNode(node_name, options) {}
+  DriverNodeWrapper(
+    const std::string & node_name, const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+  : DriverNode(node_name, options)
+  {
+  }
 
   int getNumLeds() const { return num_led_; }
   double getTimeout() const { return frame_timeout_; }
@@ -36,7 +40,6 @@ public:
   rclcpp::Time setChanel1TS(const rclcpp::Time & ts) { return chanel_1_ts_ = ts; }
   rclcpp::Time setChanel2TS(const rclcpp::Time & ts) { return chanel_2_ts_ = ts; }
 };
-
 
 class TestDriverNode : public testing::Test
 {
