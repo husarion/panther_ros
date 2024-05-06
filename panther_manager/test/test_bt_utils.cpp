22058b5924434303d94654005cacc4d01f85a067
Dawid <kmakd197@gmail.com>
2024-05-06 12:59:43 +0000

    update bt_utils tests

--- panther_manager/test/test_bt_utils.cpp
+++ panther_manager/test/test_bt_utils.cpp
@@ -13,16 +13,109 @@
 // limitations under the License.
 
 #include <any>
+#include <filesystem>
+#include <fstream>
 #include <map>
+#include <memory>
 #include <stdexcept>
 #include <string>
 
 #include "gtest/gtest.h"
 
 #include "behaviortree_cpp/tree_node.h"
+#include "rclcpp/rclcpp.hpp"
 
 #include "panther_manager/bt_utils.hpp"
 #include "panther_utils/test/test_utils.hpp"
+
+class TestRegisterBT : public testing::Test
+{
+public:
+  TestRegisterBT();
+  ~TestRegisterBT();
+
+protected:
+  void CreateBTProjectFile(const std::string & tree_xml);
+
+  std::string bt_project_path_;
+
+private:
+  const std::string simple_tree_ = R"(
+    <root BTCPP_format="4" project_name="Test">
+      <BehaviorTree ID="TestTree">
+        <Sequence>
+          <AlwaysSuccess/>
+        </Sequence>
+      </BehaviorTree>
+    </root>
+  )";
+};
+
+TestRegisterBT::TestRegisterBT()
+{
+  bt_project_path_ = testing::TempDir() + "test_bt.btproj";
+  CreateBTProjectFile(simple_tree_);
+}
+
+TestRegisterBT::~TestRegisterBT() { std::filesystem::remove(bt_project_path_); }
+
+void TestRegisterBT::CreateBTProjectFile(const std::string & tree_xml)
+{
+  std::ofstream out(bt_project_path_);
+  if (out.is_open()) {
+    out << tree_xml;
+    out.close();
+  }
+}
+
+TEST_F(TestRegisterBT, RegisterBehaviorTreeInvalidPlugin)
+{
+  BT::BehaviorTreeFactory factory;
+
+  EXPECT_THROW(
+    panther_manager::bt_utils::RegisterBehaviorTree(factory, bt_project_path_, {"invalid_bt_node"}),
+    BT::RuntimeError);
+}
+
+TEST_F(TestRegisterBT, RegisterBehaviorTree)
+{
+  BT::BehaviorTreeFactory factory;
+
+  EXPECT_NO_THROW(panther_manager::bt_utils::RegisterBehaviorTree(
+    factory, bt_project_path_, {"tick_after_timeout_bt_node", "signal_shutdown_bt_node"}));
+
+  // check if nodes were registered
+  auto nodes = factory.manifests();
+  EXPECT_TRUE(nodes.find("TickAfterTimeout") != nodes.end());
+  EXPECT_TRUE(nodes.find("SignalShutdown") != nodes.end());
+
+  // check if tree was registered
+  auto trees = factory.registeredBehaviorTrees();
+  EXPECT_TRUE(std::find(trees.begin(), trees.end(), "TestTree") != trees.end());
+}
+
+TEST_F(TestRegisterBT, RegisterBehaviorTreeROS)
+{
+  BT::BehaviorTreeFactory factory;
+
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("test_node");
+
+  EXPECT_NO_THROW(panther_manager::bt_utils::RegisterBehaviorTree(
+    factory, bt_project_path_, {}, node,
+    {"call_trigger_service_bt_node", "call_set_bool_service_bt_node"}));
+
+  // check if nodes were registered
+  auto nodes = factory.manifests();
+  EXPECT_TRUE(nodes.find("CallTriggerService") != nodes.end());
+  EXPECT_TRUE(nodes.find("CallSetBoolService") != nodes.end());
+
+  // check if tree was registered
+  auto trees = factory.registeredBehaviorTrees();
+  EXPECT_TRUE(std::find(trees.begin(), trees.end(), "TestTree") != trees.end());
+
+  rclcpp::shutdown();
+}
 
 TEST(TestBTUtils, CreateBTConfigInvalidItem)
 {
