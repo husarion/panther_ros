2ca82987cd368e9533f613ab2ad813f2c882a7f1
Jakub Delicat <jakub.delicat@husarion.com>
2024-08-26 07:31:00 +0000

    Fixed copyright
    Signed-off-by: Jakub Delicat <jakub.delicat@husarion.com>

--- panther_manager/include/panther_manager/plugins/action/dock_robot_node.hpp
+++ panther_manager/include/panther_manager/plugins/action/dock_robot_node.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Open Navigation LLC
+// Copyright 2024 Husarion sp. z o.o.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
