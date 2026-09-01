dd10d3c4fbc87f6574e620081de72810ad094fe0
Ben McCann <322311+benmccann@users.noreply.github.com>
2026-09-01 09:00:58 -0700

    Correct header inclusion

--- husarion_ugv_lights/src/lights_driver_node.cpp
+++ husarion_ugv_lights/src/lights_driver_node.cpp
@@ -32,7 +32,7 @@
 #include "husarion_ugv_msgs/srv/set_led_brightness.hpp"
 
 #include "husarion_ugv_lights/apa102.hpp"
-#include "husarion_ugv_lights/lights_controller_parameters.hpp"
+#include "husarion_ugv_lights/lights_driver_parameters.hpp"
 
 namespace husarion_ugv_lights
 {
